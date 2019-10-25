fc1a4099acf2b24a13424be76bbf5824f1377a9a
Arvid van de Crommert <arvidvdc@gmail.com>
2019-10-25 10:03:02 +0200

    Initele uitvoering opdracht

---
+++ app.js
@@ -0,0 +1,55 @@
+var express=require("express");
+var app=express();
+var bronvermelding = '<p style="text-align: center; margin: 25px; padding: 0;"> <span style="border: solid 1px; margin: 0; padding: 0 5px;">Based on course "The Web Developer Bootcamp | Udemy" from Colt Steele</span></p>'
+
+app.get("/",function(req,res){
+    res.send("Hi there, welcome to my assignment!" +bronvermelding);
+});
+
+app.get("/speak/:animal",function(req,res){
+    var animal = req.params.animal.toLowerCase();
+    var geluid
+
+    if(animal=="pig"){
+        geluid="Oink";
+    } else if (animal=="cow"){
+        geluid="Moo";
+    } else if (animal=="dog"){
+        geluid="Woof Woof";
+    } else {
+        res.send("Sorry, page not found... What are you doing with your life?"  +bronvermelding);
+    };
+    res.send("The " + animal + " says " + geluid  +bronvermelding);
+});
+
+app.get("/repeat/:repeater/:count",(request,response) => {
+    var repeat=request.params.repeater.toLowerCase();
+    var desire = repeatFunction(request.params.repeater.toLowerCase(),request.params.count);
+    
+    if(repeat=="hello"){
+        response.send(desire  +bronvermelding);
+    } else if(repeat=="blah"){
+        response.send(desire  +bronvermelding);
+    } else {
+        response.send("Sorry, page not found... What are you doing with your life?"  +bronvermelding);
+    };
+});
+
+app.get("*",function(req,res){
+    res.send("Sorry, page not found... What are you doing with your life?"  +bronvermelding);
+});
+
+
+function repeatFunction(str,num){
+    var result="";
+    for(i=0;i<num;i++){
+        // console.log(str)
+        result+= str +" ";
+    };
+    return result;
+};
+
+app.listen(3000, () => console.log("Express Applicatie gestart op poort 3000"));
+
+
+// Based on course "The Web Developer Bootcamp | Udemy" from Colt Steele
